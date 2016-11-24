267072ad34de354ccef8df6c3a302cce9122890a
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-11-24 16:27:34 +0100

    refactored Circle to extend RegularPolygon

--- src/java/com/mikosik/jsolid/d2/Circle.java
+++ src/java/com/mikosik/jsolid/d2/Circle.java
@@ -1,31 +1,15 @@
 package com.mikosik.jsolid.d2;
 
-import static com.mikosik.jsolid.JSolid.v;
 import static java.lang.Math.acos;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public final class Circle implements Polygon {
+public final class Circle extends RegularPolygon {
   private static final double PI_X_2 = 2 * Math.PI;
   private static double DELTA = 0.001;
   private static int MIN_VERTEX_COUNT = 8;
   private static double MIN_ANGLE_STEP = 2 * Math.PI / MIN_VERTEX_COUNT;
 
-  private final List<Vector2> vertexes;
-
   public Circle(double radius) {
-    this.vertexes = generateVertexes(radius);
-  }
-
-  private static List<Vector2> generateVertexes(double radius) {
-    int vertexCount = vertexCount(radius, PI_X_2);
-    ArrayList<Vector2> result = new ArrayList<>();
-    for (int i = 0; i < vertexCount; i++) {
-      double angle = (i * (PI_X_2)) / vertexCount;
-      result.add(v(Math.cos(angle) * radius, Math.sin(angle) * radius));
-    }
-    return result;
+    super(radius, vertexCount(radius, PI_X_2));
   }
 
   public static int vertexCount(double radius, double circleAngle) {
@@ -39,9 +23,5 @@ public final class Circle implements Polygon {
     } else {
       return MIN_ANGLE_STEP;
     }
-  }
-
-  public List<Vector2> vertexes() {
-    return vertexes;
   }
 }
