8c040e12ad37d96c148414d78e445f2a9d09f0ca
Marcin Mikosik <marcin.mikosik@gmail.com>
2016-12-15 17:17:04 +0100

    added prism(Polygon, double)

--- src/java/com/mikosik/jsolid/JSolid.java
+++ src/java/com/mikosik/jsolid/JSolid.java
@@ -124,6 +124,10 @@ public class JSolid {
     return prism(circle(radius), zRange);
   }
 
+  public static Prism prism(Polygon base, double zRange) {
+    return new Prism(base, range(zRange));
+  }
+
   public static Prism prism(Polygon base, Range zRange) {
     return new Prism(base, zRange);
   }
