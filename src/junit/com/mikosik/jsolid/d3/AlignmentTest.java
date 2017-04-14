797efc69bb684e9c652f6aebc2a20d0a89f95877
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-04-14 15:49:17 +0200

    refactored AlignmentTest

--- src/junit/com/mikosik/jsolid/d3/AlignmentTest.java
+++ src/junit/com/mikosik/jsolid/d3/AlignmentTest.java
@@ -1,8 +1,9 @@
 package com.mikosik.jsolid.d3;
 
 import static com.mikosik.jsolid.JSolid.cuboid;
+import static com.mikosik.jsolid.JSolid.maxX;
+import static com.mikosik.jsolid.JSolid.minX;
 import static com.mikosik.jsolid.JSolid.vx;
-import static com.mikosik.jsolid.JSolid.x;
 import static org.testory.Testory.given;
 import static org.testory.Testory.thenReturned;
 import static org.testory.Testory.when;
@@ -14,14 +15,14 @@ public class AlignmentTest {
 
   @Test
   public void alignment() throws Exception {
-    given(alignment = new Alignment(x().max(), x().min(), 0));
+    given(alignment = new Alignment(maxX(), minX(), 0));
     when(alignment.alignShiftFor(cuboid(3, 7, 11), cuboid(5, 13, 17)));
     thenReturned(vx(4));
   }
 
   @Test
   public void alignment_with_shift() throws Exception {
-    given(alignment = new Alignment(x().max(), x().min(), 3));
+    given(alignment = new Alignment(maxX(), minX(), 3));
     when(alignment.alignShiftFor(cuboid(3, 7, 11), cuboid(5, 13, 17)));
     thenReturned(vx(7));
   }
