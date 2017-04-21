808f86c114df5cc2f29e385c7069c2c997ff92be
Marcin Mikosik <marcin.mikosik@gmail.com>
2017-04-21 15:14:00 +0200

    added tests to AddTest

--- src/junit/com/mikosik/jsolid/d3/AddTest.java
+++ src/junit/com/mikosik/jsolid/d3/AddTest.java
@@ -4,7 +4,11 @@ import static com.mikosik.jsolid.JSolid.align;
 import static com.mikosik.jsolid.JSolid.cuboid;
 import static com.mikosik.jsolid.JSolid.maxX;
 import static com.mikosik.jsolid.JSolid.nothing;
+import static com.mikosik.jsolid.JSolid.range;
+import static com.mikosik.jsolid.JSolid.v;
 import static com.mikosik.jsolid.util.ExceptionMatcher.exception;
+import static com.mikosik.jsolid.util.SolidMatcher.matchesSolid;
+import static org.testory.Testory.given;
 import static org.testory.Testory.thenReturned;
 import static org.testory.Testory.thenThrown;
 import static org.testory.Testory.when;
@@ -12,6 +16,42 @@ import static org.testory.Testory.when;
 import org.junit.Test;
 
 public class AddTest {
+  private Cuboid solid;
+
+  @Test
+  public void add_two_cuboid_halves() throws Exception {
+    given(solid = cuboid(range(-1, 0), 2, 2));
+    when(() -> solid.add(cuboid(range(0, 1), 2, 2)));
+    thenReturned(matchesSolid(
+        v(-1, 1, 1),
+        v(-1, 1, -1),
+        v(-1, -1, 1),
+        v(-1, -1, -1),
+        v(0, 1, 1),
+        v(0, 1, -1),
+        v(0, -1, 1),
+        v(0, -1, -1),
+        v(1, 1, 1),
+        v(1, 1, -1),
+        v(1, -1, 1),
+        v(1, -1, -1)));
+  }
+
+  @Test
+  public void add_itself_returns_itself() throws Exception {
+    given(solid = cuboid(2, 2, 2));
+    when(() -> solid.add(cuboid(2, 2, 2)));
+    thenReturned(matchesSolid(
+        v(-1, 1, 1),
+        v(-1, 1, -1),
+        v(-1, -1, 1),
+        v(-1, -1, -1),
+        v(1, 1, 1),
+        v(1, 1, -1),
+        v(1, -1, 1),
+        v(1, -1, -1)));
+  }
+
   @Test
   public void adding_aligned_nothing_succeeds() throws Exception {
     when(() -> cuboid(1, 2, 3).add(nothing(), align(maxX())).sides());
